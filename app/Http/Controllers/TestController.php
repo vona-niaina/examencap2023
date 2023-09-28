1a8c320d37574d2ea017354e2415ef59c193ad90
vona-niaina <vonaniaina1@gmail.com>
2023-09-28 23:21:15 +0200

    nahavita page accueil aho

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Inscription;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
 
 class TestController extends Controller
@@ -28,6 +30,7 @@ class TestController extends Controller
         return view ('test.foo');
     }
 
+    //adminIndex
     public function adminIndex(){
         if(!Gate::allows('access-admin')){
             abort('403');
@@ -53,11 +56,91 @@ class TestController extends Controller
                 
             $usersAdmisDefinitif= $usersReussitEcrit->intersect($usersReussitPratique);
             $nombreUsersAdmisDefinitif= count($usersAdmisDefinitif);
-            // dd($nombreUsersAdmisDefinitif);    
+            // dd($nombreUsersAdmisDefinitif);   
+            
+            
+            //users qui a un echec en ecrit 
+            $usersEchecEcrit= Inscription::where('reussitExamen', 'non')
+                ->whereIn('examen_id', function ($query){
+                    $query->select('id')
+                    ->from('examens')
+                    ->where('typeExamen', 'ECRIT');
+                })
+                ->pluck('user_id');
+            $nombreEchecEcrit= count($usersEchecEcrit);
+          
+            //users qui a un echec pratique
+            $usersEchecPratique= Inscription::where('reussitExamen', 'non')
+            ->whereIn('examen_id', function ($query){
+                $query->select('id')
+                ->from('examens')
+                ->where('typeExamen', 'PRATIQUE');
+            })
+            ->pluck('user_id');
+            $nombreEchecPratique= count($usersEchecPratique);
+
+            //users inscrits total
+            $usersInscritsTotal = Inscription::count();
+           
+            ///users qui ont des inscriptions
+            $usersAvecInscriptions= Inscription::distinct('user_id')->pluck('user_id');
+            $nombreUsersAvecInscriptions= count($usersAvecInscriptions);
+            
+            //users sans inscriptions
+            $nombreUsersSansInscriptions= User::whereNotIn('id', $usersAvecInscriptions)->count();
+            
+            // %reussitParCisco: bar 
+            //
+            $ciscos= DB::table('ciscos')
+                ->select('id', 'nomCisco') 
+                ->get();
+            
+            $pourcentagesReussite = [];
+
+            foreach ($ciscos as $cisco) {
+                $idCisco = $cisco->id;
+                $nomCisco = $cisco->nomCisco;
+
+                //Nombre total d'inscriptions pour ce Cisco
+                $totalInscriptions = DB::table('inscriptions')
+                    ->whereIn('user_id', function ($query) use ($idCisco) {
+                        $query->select('users.id')
+                            ->from('users')
+                            ->join('etabs', 'users.etab_id', '=', 'etabs.id')
+                            ->join('zaps', 'etabs.zap_id', '=', 'zaps.id')
+                            ->where('zaps.cisco_id', $idCisco);
+                    })
+                    ->count();
+
+                //nombre d'inscriptions réussites aux types écrits et pratique pour ce Cisco
+                $nombreReussitesEcritPratique = DB::table('inscriptions')
+                    ->where('reussitExamen', 'oui')
+                    ->whereIn('examen_id', function ($query) {
+                        $query->select('id')
+                        ->from('examens')
+                        ->whereIn('typeExamen', ['ECRIT', 'PRATIQUE']);
+                    })
+                    ->count();
+
+                //calculer le pourcentage  de réussite 
+                $pourcentageReussite = $totalInscriptions > 0 ? ($nombreReussitesEcritPratique / $totalInscriptions) * 100 : 0;
+                $pourcentagesReussite[$idCisco] = $pourcentageReussite;   
+
+                //$pourcentafesReussite contient maintenant le pourcentage de reussite pour chaque Cisco    
+            }
+            // dd($pourcentagesReussite);
+
+            //users qui ont réussi définitivement par cisco(%sur les users qui ont une inscription sur ce cisco)
+            
+
+            //reussitParAnnee: line
+            
 
 
         return view ('adminAcc');
-    }
+    }//end func
+
+
 
     public function cliIndex(){
         if(!Gate::allows('access-cli')){
